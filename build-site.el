911eb03e5705271d7e8f8a17ab11ed55a5f15480
Daniele Moser <dnlmsr0@gmail.com>
2022-03-05 16:09:33 +0100

    Fixed code blocks not evaluating

--- build-site.el
+++ build-site.el
@@ -1,4 +1,11 @@
 (require 'ox-publish)
+
+(setq org-confirm-babel-evaluate nil)
+(setq org-export-babel-evaluate t)
+(setq org-export-use-babel t)
+
+(org-babel-do-load-languages 'org-babel-load-languages'((shell . t)))
+
 (setq org-publish-project-alist
       '(("mosis.xyz"
          :recursive t
