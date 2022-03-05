dfc8738896c37c2912a85a3bd747538866c42f3e
Daniele Moser <dnlmsr0@gmail.com>
2022-03-05 17:54:47 +0100

    Added simple CSS

--- build-site.el
+++ build-site.el
@@ -9,6 +9,7 @@
 (setq org-html-validation-link nil 
       org-html-head-include-scripts nil
       org-html-head-include-default-style nil
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\">"
       )
 (setq org-publish-project-alist
       '(("mosis.xyz"
