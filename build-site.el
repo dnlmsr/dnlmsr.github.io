6fbf40397ca32285c7ec022a2cc29be9b763bf69
Daniele Moser <dnlmsr0@gmail.com>
2022-03-05 17:54:27 +0100

    Improved html output

--- build-site.el
+++ build-site.el
@@ -6,12 +6,22 @@
 
 (org-babel-do-load-languages 'org-babel-load-languages'((shell . t)))
 
+(setq org-html-validation-link nil 
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
+      )
 (setq org-publish-project-alist
       '(("mosis.xyz"
          :recursive t
          :base-directory "./"
          :publishing-directory "./public"
-         :publishing-function org-html-publish-to-html)))
+         :publishing-function org-html-publish-to-html
+         :with-author t
+         :with-creator t
+         :section-numbers nil
+         :time-stamp-file t
+         :language en
+         )))
 
 (org-publish-all t)
 
