94848b77254b7492a2373e32c5097802becfeb87
Daniele Moser <dnlmsr0@gmail.com>
2022-03-12 17:32:43 +0100

    Fixed typo

--- build-site.el
+++ build-site.el
@@ -9,7 +9,7 @@
 (setq org-html-validation-link nil
       org-html-head-include-scripts nil
       org-html-head-include-default-style nil
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\">"
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\"/>"
       )
 
 (defun html/preamble (plist)
